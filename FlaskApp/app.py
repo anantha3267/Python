ee4370c301787e7a86afae35dcdcee425fc259c5
anantha3267 <ananthamchiranjeevi@gmail.com>
2024-10-24 12:02:38 -0400

    Flask Final Version

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Dockerized Flask Built on 10/24!'
+    return 'Hello, Dockerized Flask Final Version 10/24/24!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
